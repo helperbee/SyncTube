975ee93721f2f716d708811fae0ad902f1eb9051
M <amfrombr@gmail.com>
2023-09-05 15:01:10 -0400

    added pinning functionality to comments for now

--- dist/Components/Comment.jsx
+++ dist/Components/Comment.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Typography } from 'antd';
+import { PushpinOutlined, PushpinFilled } from '@ant-design/icons';
 
+const { Paragraph } = Typography;
 
 
 let Comment = ({comment}) => {
+    const [isHovered, setIsHovered] = React.useState(false);
+    const [isPinned, setIsPinned] = React.useState(false);
     return (
-        <h3>{`[${comment.time}]${comment.text}`}</h3>
+        <Paragraph style={{ textAlign: 'left' }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}>
+        {(isHovered) ? (isPinned ? <PushpinFilled onClick={() => setIsPinned(false)} style={{cursor:'pointer'}}/> : <PushpinOutlined onClick={() => setIsPinned(true)} style={{cursor:'pointer'}}/>) : ''}{/*rotate-135?*/}
+        {comment.text}
+      </Paragraph>
     );
 };
 
